e58a02511c3cbd59489dc451b5057aa634706879
hutchings90 <hutchings90@gmail.com>
2018-06-07 19:40:28 -0600

    Done!!!!!!!!!!!!!

--- Problem2BayesianNetwork.py
+++ Problem2BayesianNetwork.py
@@ -1,9 +1,9 @@
 class BayesianNetwork:
     bm = [
-        [True, True, .42],
-        [False, True, .58],
-        [True, False, .3],
-        [False, False, .7]
+        [True, True, .9],
+        [False, True, .1],
+        [True, False, .01],
+        [False, False, .99]
     ]
     dm = [
         [True, True, .5],
@@ -12,49 +12,75 @@ class BayesianNetwork:
         [False, False, 1]
     ]
     jb = [
-        [True, True, .05],
-        [False, True, .95],
-        [True, False, .4],
-        [False, False, .6]
+        [True, True, .01],
+        [False, True, .99],
+        [True, False, .95],
+        [False, False, .05]
     ]
     wjbd = [
-        [True, True, True, True, .3],
-        [False, True, True, True, .7],
-        [True, False, True, True, .65],
-        [False, False, True, True, .35],
-        [True, True, False, True, .65],
-        [False, True, False, True, .35],
+        [True, True, True, True, .1],
+        [False, True, True, True, .9],
+        [True, False, True, True, .6],
+        [False, False, True, True, .4],
+        [True, True, False, True, .7],
+        [False, True, False, True, .3],
         [True, False, False, True, 1],
         [False, False, False, True, 0],
-        [True, True, True, False, .1],
-        [False, True, True, False, .9],
-        [True, False, True, False, .6],
-        [False, False, True, False, .4],
-        [True, True, False, False, .6],
-        [False, True, False, False, .4],
-        [True, False, False, False, .9],
-        [False, False, False, False, .1]
+        [True, True, True, False, 0],
+        [False, True, True, False, 1],
+        [True, False, True, False, .4],
+        [False, False, True, False, .6],
+        [True, True, False, False, .5],
+        [False, True, False, False, .5],
+        [True, False, False, False, 1],
+        [False, False, False, False, 0]
     ]
 
-    def prob(self, w, m):
-        print('w:', w, '\nm:', m)
+    def prob(self):
+        retVal = []
         total = 0
         for pwjbd in BayesianNetwork.wjbd:
-            if pwjbd[0] == w:
-                j = pwjbd[1]
-                b = pwjbd[2]
-                d = pwjbd[3]
-                for pbm in BayesianNetwork.bm:
-                    if pbm[0] == b and pbm[1] == m:
-                        for pdm in BayesianNetwork.dm:
-                            if pdm[0] == d and pdm[1] == m:
+            if pwjbd[0] == False:
+                pd = pwjbd[3]
+                pb = pwjbd[2]
+                pj = pwjbd[1]
+                for pdm in BayesianNetwork.dm:
+                    if pdm[0] == pd and pdm[1]:
+                        for pbm in BayesianNetwork.bm:
+                            if pbm[0] == pb and pbm[1]:
                                 for pjb in BayesianNetwork.jb:
-                                    if pjb[0] == j:
-                                        partial = pbm[2] * pwjbd[4] * pdm[2] * pjb[2]
+                                    if pjb[0] == pj and pjb[1] == pb:
+                                        partial = pwjbd[4] * pdm[2] * pbm[2] * pjb[2]
                                         total += partial
-                                        # print(partial, pbm, pwjbd, pdm, pjb)
-        return round(total * 100)
+                                        # print(pwjbd, pdm, pbm, pjb)
+        retVal.append({
+            'prob': 'P[W|M](False|True): ',
+            'val': round(total * 100)
+        })
+        total = 0
+        for pwjbd in BayesianNetwork.wjbd:
+            if pwjbd[0] == False:
+                pd = pwjbd[3]
+                pb = pwjbd[2]
+                pj = pwjbd[1]
+                for pdm in BayesianNetwork.dm:
+                    if pdm[0] == pd and pdm[1] == False:
+                        for pbm in BayesianNetwork.bm:
+                            if pbm[0] == pb and pbm[1] == False:
+                                for pjb in BayesianNetwork.jb:
+                                    if pjb[0] == pj and pjb[1] == pb:
+                                        partial = pwjbd[4] * pdm[2] * pbm[2] * pjb[2]
+                                        total += partial
+                                        # print(total, partial, pdm, pbm, pjb)
+        retVal.append({
+            'prob': 'P[W|M](False|False): ',
+            'val': round(total * 100)
+        })
+        return retVal
 
 b = BayesianNetwork()
-print('****************************************************************\n', b.prob(False, True), '\n****************************************************************\n')
-print('****************************************************************\n', b.prob(False, False), '\n****************************************************************\n')
+print('****************************************************************')
+results = b.prob()
+for result in results:
+    print(result['prob'], result['val'])
+print('****************************************************************\n')
--- Problem3BayesianNetwork.py
+++ Problem3BayesianNetwork.py
@@ -6,33 +6,32 @@ class BayesianNetwork:
         [False, False, .1]
     ]
     dvfrs = [
-        [True, True, .3],
-        [False, True, .7],
+        [True, True, .1],
+        [False, True, .9],
         [True, False, .9],
         [False, False, .1]
     ]
     ppshg = [
-        [True, True, .3],
-        [False, True, .7],
+        [True, True, .7],
+        [False, True, .3],
         [True, False, .6],
         [False, False, .4],
     ]
     frsm = [
-        [True, True, .7],
-        [False, True, .3],
+        [True, True, .9],
+        [False, True, .1],
         [True, False, .2],
         [False, False, .8]
     ]
     fm = [
-        [True, True, .3],
-        [False, True, .7],
-        [True, False, .6],
-        [False, False, .4]
+        [True, True, .49],
+        [False, True, .51],
+        [True, False, .7],
+        [False, False, .3]
     ]
-
     kppm = [
-        [True, True, True, .3],
-        [False, True, True, .7],
+        [True, True, True, .9],
+        [False, True, True, .1],
         [True, False, True, .6],
         [False, False, True, .4],
         [True, True, False, .3],
@@ -40,10 +39,19 @@ class BayesianNetwork:
         [True, False, False, .6],
         [False, False, False, .4],
     ]
+    oshgf = [
+        [True, True, True, .25],
+        [False, True, True, .75],
+        [True, False, True, .1],
+        [False, False, True, .9],
+        [True, True, False, .95],
+        [False, True, False, .05],
+        [True, False, False, 0],
+        [False, False, False, 1],
+    ]
 
     def prob(self):
         retVal = []
-
         total = 0
         for pdvfrs in BayesianNetwork.dvfrs:
             if pdvfrs[0]:
@@ -55,7 +63,7 @@ class BayesianNetwork:
                         # print(total, partial, pfrs, pfrsm)
         retVal.append({
             'prob': 'P[DV|M](True|True): ',
-            'val': total
+            'val': round(total * 100)
         })
         total = 0
         for pdvfrs in BayesianNetwork.dvfrs:
@@ -68,7 +76,7 @@ class BayesianNetwork:
                         # print(total, partial, pfrs, pfrsm)
         retVal.append({
             'prob': 'P[DV|M](False|True)]: ',
-            'val': total
+            'val': round(total * 100)
         })
         total = 0
         for paf in BayesianNetwork.af:
@@ -81,7 +89,7 @@ class BayesianNetwork:
                         # print(total, partial, paf, pfm)
         retVal.append({
             'prob': 'P[A|M](True|True): ',
-            'val': total
+            'val': round(total * 100)
         })
         total = 0
         for paf in BayesianNetwork.af:
@@ -94,7 +102,7 @@ class BayesianNetwork:
                         # print(total, partial, paf, pfm)
         retVal.append({
             'prob': 'P[A|M](False|True): ',
-            'val': total
+            'val': round(total * 100)
         })
         total = 0
         for pkppm in BayesianNetwork.kppm:
@@ -107,7 +115,7 @@ class BayesianNetwork:
                         # print(total, partial, kppm, ppshg)
         retVal.append({
             'prob': 'P[K|M,SHG](True|True,True): ',
-            'val': total
+            'val': round(total * 100)
         })
         total = 0
         for pkppm in BayesianNetwork.kppm:
@@ -120,7 +128,33 @@ class BayesianNetwork:
                         # print(total, partial, kppm, ppshg)
         retVal.append({
             'prob': 'P[K|M,SHG](False|True,True): ',
-            'val': total
+            'val': round(total * 100)
+        })
+        total = 0
+        for poshgf in BayesianNetwork.oshgf:
+            if poshgf[0] and poshgf[1]:
+                pf = poshgf[2]
+                for pfm in BayesianNetwork.fm:
+                    if pfm[0] == pf and pfm[1]:
+                        partial = poshgf[3] * pfm[2]
+                        total += partial
+                        # print(total, partial, kppm, ppshg)
+        retVal.append({
+            'prob': 'P[O|SHG,M](True|True,True): ',
+            'val': round(total * 100)
+        })
+        total = 0
+        for poshgf in BayesianNetwork.oshgf:
+            if poshgf[0] == False and poshgf[1]:
+                pf = poshgf[2]
+                for pfm in BayesianNetwork.fm:
+                    if pfm[0] == pf and pfm[1]:
+                        partial = poshgf[3] * pfm[2]
+                        total += partial
+                        # print(total, partial, kppm, ppshg)
+        retVal.append({
+            'prob': 'P[O|SHG,M](False|True,True): ',
+            'val': round(total * 100)
         })
         return retVal
 
@@ -130,7 +164,7 @@ results = b.prob()
 total = 0
 i = 1
 for result in results:
-    print(result['prob'] + ':', result['val'])
+    print(result['prob'], result['val'])
     total += result['val']
     if i % 2 == 0:
         print(total)
